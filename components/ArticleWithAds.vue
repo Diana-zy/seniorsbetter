9af98f9ccbc703b1ab55823f81e75b5b64383fe7
Diana-zy <zhangying@himobi.cc>
2026-04-14 15:33:19 +0800

    feat: update ArticleWithAds.vue to SEO version

--- components/ArticleWithAds.vue
+++ components/ArticleWithAds.vue
@@ -3,7 +3,7 @@
     <template v-for="(item, index) in contentItems">
       <!-- eslint-disable-next-line vue/no-v-html -->
       <div v-if="item.type === 'content'" :key="`content-${index}`" v-html="item.content" />
-<!--      <GoogleAd v-else :key="`ad-${index}`" :ad-slot="item.slot" />-->
+      <GoogleAd v-else :key="`ad-${index}`" :ad-slot="item.slot" />
     </template>
   </div>
 </template>
@@ -26,18 +26,6 @@ export default {
         {
           slot: "9106902700"
         }
-        // {
-        //   slot: "4524659860"
-        // },
-        // {
-        //   slot: "8648298782"
-        // },
-        // {
-        //   slot: "3284393095"
-        // },
-        // {
-        //   slot: "1971311422"
-        // }
       ]
     }
   },
@@ -51,23 +39,19 @@ export default {
       const maxAds = this.adConfigs.length;
 
       parts.forEach((part) => {
-        if (!part.trim()) return; // 跳过空字符串
+        if (!part.trim()) return;
 
-        // 添加内容
         items.push({
           type: "content",
           content: part
         });
 
-        // 如果不是p标签，不计算字符数和插入广告
         if (!part.startsWith("<p")) return;
         if (adIndex >= maxAds) return;
 
-        // 计算纯文本长度
         const textContent = part.replace(/<[^>]+>/g, "");
         charCount += textContent.length;
 
-        // 检查是否需要插入广告
         if (
           charCount >= this.charInterval &&
           (charCount - lastAdCharCount >= this.charInterval || lastAdCharCount === 0)
