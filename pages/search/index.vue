09151ec997a16510490e4f68783285311c27a67b
Diana-zy <zhangying@himobi.cc>
2026-04-21 11:37:35 +0800

    rebuild: add pages/search/index.vue with English UI text

---
+++ pages/search/index.vue
@@ -0,0 +1,193 @@
+<template>
+  <div class="page">
+    <Header />
+    <main>
+      <div class="layout-left">
+        <common-page-label :title="`&quot;${input}&quot; Search Results`" />
+        <div id="afscontainer1"> </div>
+        <div id="relatedsearches1"> </div>
+        <h3 class="title-h3">{{ searchTitle }}</h3>
+        <section>
+          <item-search-result v-for="(item, i) in searchResultNews" :key="i" :item="item">
+          </item-search-result>
+        </section>
+      </div>
+      <div class="layout-right">
+        <right-side-box :rec-news="trendingNews?.list" :trending-news="recNews?.list" />
+      </div>
+    </main>
+    <FooterSeo />
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ $axios, env }) {
+    try {
+      const [recNewsResponse, trendingNewsResponse] = await Promise.all([
+        $axios.$get("/api/article/menu", {
+          params: {
+            site_id: env.SITE_ID,
+            mod_id: "rec"
+          }
+        }),
+        $axios.$get("/api/article/get_all_articles", {
+          params: {
+            site_id: env.SITE_ID,
+            size: 4,
+            page: 1
+          }
+        })
+      ]);
+      return {
+        recNews: recNewsResponse,
+        trendingNews: trendingNewsResponse
+      };
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  },
+  data() {
+    return {
+      searchResultNews: [],
+      input: "",
+      channelId: "",
+      isShowResults: false
+    };
+  },
+  computed: {
+    searchTitle() {
+      const title = this.isShowResults
+        ? `Found ${this.searchResultNews.length} results for "${this.input}"`
+        : "Loading search results...";
+      return title;
+    }
+  },
+  mounted() {
+    const searchParams = new URLSearchParams(window.location.search);
+    this.channelId = searchParams.has("channel") ? searchParams.get("channel") : "";
+    if (searchParams.has("from") && searchParams.get("from") === "detail") {
+      window.fromDetailId = window.getCookie("SEO_detail");
+    }
+    window.setCookie("SEO_detail", "");
+
+    this.input = this.$route.query.query || "";
+    this.input && this.addAdSense();
+    this.input && this.searchNews();
+  },
+  methods: {
+    addAdSense() {
+      setTimeout(() => {
+        window.pushEventParamsToGtm("Q_AR");
+        window.trackEventToPixel("Q_AR");
+        this.addAdSenseScript();
+      }, 0);
+    },
+    async searchNews() {
+      try {
+        const response = await this.$axios.$post("/api/article/search", {
+          site_id: process.env.SITE_ID,
+          key: this.input
+        });
+
+        this.searchResultNews = response.list;
+        this.isShowResults = true;
+      } catch (error) {
+        this.isShowResults = true;
+        console.error("Error fetching data:", error);
+      }
+    },
+    addAdSenseScript() {
+      const queryString = this.input;
+
+      const channelId = window.getParam("channel");
+      const from = window.getParam("from");
+      const hiSource = window.getParam("hi_source");
+      const hiPc = window.getParam("hi_pc");
+      const resultsPageBaseUrl = window.getResultsPageUrl({
+        channel: channelId,
+        from,
+        hi_source: hiSource,
+        hi_pc: hiPc
+      });
+      const adSenseConfig = {
+        channel: channelId,
+        pubId: "partner-pub-6612490456597819",
+        query: queryString,
+        styleId: "6462282781",
+        adsafe: "low",
+        ivt: false,
+        resultsPageBaseUrl,
+        resultsPageQueryParam: "query"
+      };
+
+      const adLoadedCallback =
+        (eventName, additionalData = {}) =>
+        (loaded, response) => {
+          if (response) {
+            // eslint-disable-next-line no-undef
+            dataLayer.push({ event: eventName, ...additionalData });
+          }
+        };
+
+      const adblock1 = {
+        container: "afscontainer1",
+        number: 8,
+        adLoadedCallback: (loaded, e) => {
+          if (e) {
+            window.trackEventToPixel("C_AR");
+            window.pushEventParamsToGtm("C_AR");
+            const hi_user_source = getValueByURLOrCookie("hi_source");
+            if (hi_user_source === "unknown") {
+              window.dataLayer.push({
+                event: "Detail_C_AR_C_SEO",
+                SEO_detail: window.fromDetailId || ""
+              });
+            }
+            try {
+              const element = document.getElementById("master-1");
+              const height = parseFloat(element.style.height);
+              const result = Math.round(height / 456);
+              // eslint-disable-next-line no-undef
+              dataLayer.push({ event: "C_AR_IN", num: result, query: queryString });
+            } catch (error) {
+              console.error(error);
+            }
+          } else {
+            // eslint-disable-next-line no-undef
+            dataLayer.push({ event: "FF_AR", query: queryString });
+          }
+        }
+      };
+
+      const rsblock1 = (() => {
+        const baseConfig = {
+          container: "relatedsearches1",
+          relatedSearches: 5,
+          adLoadedCallback: adLoadedCallback("C_AC", { query: queryString })
+        };
+        return baseConfig;
+      })();
+
+      // eslint-disable-next-line no-undef
+      _googCsa("ads", adSenseConfig, adblock1, rsblock1);
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+main {
+  padding-bottom: 32px;
+  border-bottom: 1px solid #ececee;
+}
+.title-h3 {
+  color: $font4;
+}
+@media screen and (max-width: 750px) {
+  main {
+    padding-bottom: vw(32);
+    border-bottom: vw(2) solid #ececee;
+  }
+}
+</style>
