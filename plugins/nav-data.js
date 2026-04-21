e1752262d191ba1215e0975baccc8933d3e5500e
Diana-zy <zhangying@himobi.cc>
2026-04-21 10:46:26 +0800

    rebuild: add plugins from koureishalife

---
+++ plugins/nav-data.js
@@ -0,0 +1,17 @@
+export default async function ({ $axios, env, isServer }, inject) {
+  try {
+    const data = await $axios.$get("/api/article/get_all_seo_category", {
+      params: {
+        site_id: env.SITE_ID
+      }
+    });
+    inject("navData", data);
+    inject("sameAs", [
+      "https://x.com/seniorsbetter",
+      "https://www.facebook.com/people/Seniorsbetter/61586174372459/"
+    ]);
+  } catch (error) {
+    console.error("Failed to fetch navigation data:", error);
+    inject("navData", []);
+  }
+}
