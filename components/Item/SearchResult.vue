cc7d9d0c223c3d12d8f07a81fa4b2835ea87fd71
Diana-zy <zhangying@himobi.cc>
2026-04-21 10:56:00 +0800

    rebuild: add Item sub-components (ModeNew, SearchResult, SwiperRec, TextNew)

---
+++ components/Item/SearchResult.vue
@@ -0,0 +1,54 @@
+<template>
+  <CustomLink class="item" :to="`/${item.path_v2}/`">
+    <p class="title">{{ item.name }}</p>
+    <p class="path">{{ formattedPath }}</p>
+    <p class="desc">{{ item.first_paragraph }}</p>
+  </CustomLink>
+</template>
+
+<script>
+export default {
+  props: {
+    item: {
+      type: Object,
+      required: true
+    }
+  },
+  computed: {
+    formattedPath() {
+      return `${window.location.origin}/${this.item.path_v2}/`;
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.item {
+  display: block;
+  border-bottom: 1px solid #ececee;
+  margin-bottom: 24px;
+  color: rgba($font3, 0.6);
+  .title {
+    font-family: "hem";
+    font-size: 22px;
+    line-height: 29px;
+    color: $font3;
+  }
+  .path {
+    line-height: 19px;
+    margin-top: 8px;
+    margin-bottom: 16px;
+  }
+  .desc {
+    line-height: 19px;
+    margin-bottom: 24px;
+    @include ellipsis(7);
+  }
+  &:hover {
+    .title {
+      color: $color1;
+      text-decoration: underline;
+    }
+  }
+}
+</style>
